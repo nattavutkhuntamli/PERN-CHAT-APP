f731c26fa7b939b494eb87f78702e572e21143c6
Nattavut <121441228+nattavutkhuntamli@users.noreply.github.com>
2024-07-24 10:13:50 +0700

    ีย

--- backend/src/controllers/message.controller.ts
+++ backend/src/controllers/message.controller.ts
@@ -1,5 +1,81 @@
-import { Request, Response } from "express"
+import { Request, Response } from "express";
+import prisma from "../db/prisma";
+
 
 export const sendMessage = async( req:Request, res:Response ) => {
+    try {
+        const { message } = req.body;
+        const { id:receiverId } = req.params;
+        const senderId = req.user.id;
 
+        let conversation = await prisma.conversation.findFirst( {
+            where: {
+                participantIds: {
+                    hasEvery:[senderId, receiverId]
+                }
+            },
+        })
+
+        // the very first message is being sent that's why we need to create a new convdrsation
+        if (!conversation) {
+            conversation = await prisma.conversation.create({
+                data:{
+                    participantIds:{
+                        set:[senderId, receiverId]
+                    }
+                }
+            });
+        }
+        const newMessage = await prisma.message.create({
+            data:{
+                senderId,
+                body:message,
+                conversationId:conversation.id
+            }
+        });
+        
+        if(newMessage){
+            conversation = await prisma.conversation.update({
+                where:{ id:conversation.id},
+                data:{
+                    messages:{
+                        connect:{
+                            id:newMessage.id
+                        },
+                    },
+                },
+            })
+        }
+        //socket io will go here
+        return res.status(201).json(newMessage)
+    } catch (error:any) {
+        console.error(`Error in signup controller ${error.message}`);
+        return res.status(500).json({ error: 'Internal server error' });
+    }
+}
+
+export const getMessage  = async( req:Request, res:Response) => {
+    try {
+        const { id: userToChatId} = req.params;
+        const senderId = req.user.id;
+        const conversation = await prisma.conversation.findFirst( {
+             where:{  participantIds:{ hasEvery:[senderId, userToChatId] } },
+             include:{
+                messages:{
+                    orderBy:{
+                        createdAt:'asc'
+                    }
+                }
+             }
+        } );
+
+        if(!conversation) {
+            return res.status(200).json([])
+        }
+        res.status(200).json(conversation.messages);
+        
+    } catch (error:any) {
+        console.error(`Error in signup controller ${error.message}`);
+        return res.status(500).json({ error: 'Internal server error' });
+    }
 }
